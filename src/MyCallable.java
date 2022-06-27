d8cf01a508bcf2eaf0833caf774227ba15d6f857
Bair Arshaev <ars-bair@yandex.ru>
2022-06-27 23:02:20 +0300

    Edit code 1

---
+++ src/MyCallable.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.Callable;
+
+public class MyCallable extends Thread implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        int n = 0;
+        try {
+            for (int i = 0; i < 4; i++) {
+                Thread.sleep(2500);
+                System.out.println("Всем привет! Я " + Thread.currentThread().getName());
+                n++;
+            }
+//            while(!isInterrupted()) {
+//                Thread.sleep(2500);
+//                System.out.println("Всем привет! Я " + Thread.currentThread().getName());
+//                n++;
+//            }
+        } catch (InterruptedException err) {
+
+        } finally{
+            System.out.printf("%s завершен\n", Thread.currentThread().getName());
+        }
+
+        return n;
+    }
+}
