690c699fe66ba36620b145becd378a139a2a4804
Bair Arshaev <ars-bair@yandex.ru>
2022-06-28 23:24:23 +0300

    Edit code 3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,30 @@
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        Callable<Integer> myCallable = new MyCallable();
+        ExecutorService executorService = Executors.newFixedThreadPool(4, new CustomThreadFactory());
 
-        ExecutorService threadPool = Executors.newFixedThreadPool(4, new CustomThreadFactory());
+        MyCallable t1 = new MyCallable();
+        MyCallable t2 = new MyCallable();
+        MyCallable t3 = new MyCallable();
+        MyCallable t4 = new MyCallable();
 
-        Future<Integer> task1 = threadPool.submit(myCallable);
-        Future<Integer> task2 = threadPool.submit(myCallable);
-        Future<Integer> task3 = threadPool.submit(myCallable);
-        Future<Integer> task4 = threadPool.submit(myCallable);
+        final List<MyCallable> tasks = List.of(t1, t2, t3, t4);
+        final List<Future<Integer>> futures = executorService.invokeAll(tasks);
 
-        int result = task1.get() + task2.get() +task3.get() + task4.get() + 1;
+        int result = 0;
+        for (Future<Integer> future : futures) {
+            result = result + future.get();
+        }
         System.out.println("Всего выведено в консоль "  + result + " сообщений!");
-        threadPool.shutdown();
+
+        System.out.println("Получение результата самой быстрой задачи...");
+        final int fastFuture = executorService.invokeAny(tasks);
+        System.out.println("Самая быстрая задача вывела в консоль " + fastFuture + " сообщений");
+
+        executorService.shutdown();
     }
 }
